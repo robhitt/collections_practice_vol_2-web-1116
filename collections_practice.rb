0d83cd31b39132965640871395023701d47f07b5
Robert Hitt <robhitt@gmail.com>
2016-11-28 03:43:52 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,72 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? { |word| word[0] == 'r'}
+end
+
+def contain_a(array)
+  a_array = []
+  array.each do |word|
+    if word.include?("a")
+      a_array << word
+    end
+  end
+  a_array
+end
+
+def first_wa(array)
+  array.each do |word|
+    return word if (word[0]+word[1] == "wa")
+  end
+end
+
+def remove_non_strings(array)
+  array.select { |element| element.class == String }
+end
+
+def count_elements(array)
+  array.each do |original_hash|
+    original_hash[:count] = 0
+    name = original_hash[:name]
+    array.each do |hash|
+      if hash[:name] == name
+        original_hash[:count] += 1
+      end
+    end
+  end.uniq
+end
+
+def merge_data(keys, values)
+  container = []
+  keys.each do |person_name|
+    name = person_name[:first_name]
+    values.each do |person_data|
+      if person_data[name]
+        merged_person = person_data[name]
+        merged_person[:first_name] = name
+        container << merged_person
+      end
+    end
+  end
+  container
+end
+
+def find_cool(array)
+  container = []
+  array.each do |element|
+    container << element if element[:temperature] == "cool"
+  end
+  container
+end
+
+def organize_schools(schools)
+  organized_schools = {}
+  schools.each do |name, location_hash|
+    location = location_hash[:location]
+    if organized_schools[location]
+      organized_schools[location] << name
+    else
+      organized_schools[location] = []
+      organized_schools[location] << name
+    end
+  end
+  organized_schools
+end
